d21afc6e7eb82dd5e0888ebbaeee5bdf9dbf52f6
Dione <dionebraga.work@gmail.com>
2025-03-09 19:11:19 -0300

    Segundo commit - Dashboard de Projetos Culturais

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from dash import Dash, html
-app = Dash(__name__)
-app.layout = html.H1("Dashboard AWS Free Tier")
-server = app.server
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Aplicação no Elastic Beanstalk funcionando!"
 
 if __name__ == "__main__":
-    app.run_server(host='127.0.0.1', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="127.0.0.1", port=port)
